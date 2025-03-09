a367a75d042be2406dfd1b2d841c8e4ec4e1e282
Koris <kamranhaq@protonmail.com>
2025-03-09 10:11:22 -0700

    Implemented JSON response

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  var timestamp = Date.now()
+  res.json({
+    "message": "My name is Koris",
+    'timestamp': timestamp
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
